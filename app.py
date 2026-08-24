b020cfeb8a7f9a8e81cb475b0b019717ef405f8f
Bishoy Emad <112349347+bishoyemad74@users.noreply.github.com>
2026-08-24 04:40:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ except Exception:
     HAS_PYZBAR = False
 
 # ⚠️ ضع الـ ID الحقيقي الخاص بشيت جوجل هنا بدلاً من النص المؤقت
-SPREADSHEET_ID = "ضع_الـ_ID_الحقيقي_هنا"
+SPREADSHEET_ID = "1B4Ho5U0x0TDf36bu7KqxXnMZCnvAiVxzfLthX_ga94c"
 SHEET_FULL_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/edit"
 
 # دالة الاتصال المباشر بشيت جوجل
