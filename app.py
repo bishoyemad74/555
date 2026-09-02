12ea81581305c79fc25f008dfa481c6045bc3f51
Bishoy Emad <112349347+bishoyemad74@users.noreply.github.com>
2026-09-03 00:49:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -516,7 +516,7 @@ if "attendance" in tab_dict:
 
         if st.session_state.session_start_time is not None:
             elapsed_min = int((time.time() - st.session_state.session_start_time) // 60)
-            curr_score = max(0.0, round(10.0 - (elapsed_min * 0.2), 1))
+            curr_score = max(0.0, round(10.0 - (elapsed_min * 1), 5))
             st.info(f"⏱️ زمن الاجتماع: {elapsed_min} دقيقة | درجة الحضور الآن: **{curr_score} / 10**")
         else:
             curr_score = 10.0
